a0c94e01164707ab596407744875f11cbf1bc62e
Yashwardhan Singh Chandel <ytheading10@gmail.com>
2023-04-20 18:06:59 +0530

    all Bugs fixed, now onto Pagination

--- app/controllers/allrequests_controller.rb
+++ app/controllers/allrequests_controller.rb
@@ -1,6 +1,6 @@
 class AllrequestsController < ApplicationController
     def update_accept_request
-        debugger
+        
         @update_accept= User.find(params[:user_id])
         @update_accept.allrequest.update(status: "approved")
         redirect_to root_path
@@ -8,7 +8,7 @@ class AllrequestsController < ApplicationController
 
     def update_reject_request
         @update_reject= User.find(params[:user_id])
-        @update_reject.update(status: "rejected")
+        @update_reject.allrequest.update(status: "rejected")
         redirect_to root_path
     end
 end
